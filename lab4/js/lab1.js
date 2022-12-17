d3aab5d6d90ae1bc0a687eb4355eb40431e65736
Wilfredo Lopez <wilfredolopez39@gmail.com>
2022-12-16 22:09:49 -0500

    changing web clock

--- lab4/js/lab1.js
+++ lab4/js/lab1.js
@@ -1,174 +1,51 @@
-var manualstate = 0;
-var countDownDateTime = 0;
-var startBtnstate = 0;
-var countdownMilliSeconds;
-var x, now, days, hours, minutes, seconds, milliseconds;
+const display = document.getElementById('clock');
+const audio = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-alarm-digital-clock-beep-989.mp3');
+audio.loop = true;
+let alarmTime = null;
+let alarmTimeout = null;
 
-$(document).ready(function () {
-    var audioElement = document.createElement('audio');
-    audioElement.setAttribute('src', 'sound/quietalarm.mp3');
+function updateTime() {
+    const date = new Date();
 
-    now = new Date();
-    var month = now.getMonth() + 1;
-    var day = now.getDate();
-    var output = now.getFullYear() + '-' +
-            (month < 10 ? '0' : '') + month + '-' +
-            (day < 10 ? '0' : '') + day;
-    seconds = now.getSeconds();
-    minutes = now.getMinutes();
-    var time = now.getHours() + ":" +
-            (minutes == 0 ? 00 : '') + (minutes < 10 ? 0 : '') + minutes + ":" +
-            (seconds == 0 ? 00 : '') + (seconds < 10 ? 0 : '') + seconds;
-    $("#EventDate").val(output);
-    $("#EventTime").val(time);
+    const hour = formatTime(date.getHours());
+    const minutes = formatTime(date.getMinutes());
+    const seconds = formatTime(date.getSeconds());
 
-    $("#setEventDate").click(function () {
-        var countDownDate = $("#EventDate").val();
-        var countDownTime = $("#EventTime").val();
 
-        countDownDateTime = new Date(countDownDate + " " + countDownTime);
 
-        var now = new Date();
-        var difference = (countDownDateTime - now);
-        if (difference < 0) {
-            alert("The date or time you have selected has passed, Please select a new date and time.")
-        } else {
-            alert("Alarm has been set to " + countDownDateTime);
-            alert("There's no going back now! MWAHAHA!");
-            $("#Days").val(Math.floor(difference / (1000 * 60 * 60 * 24)));
-            $("#Hours").val(Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-            $("#Minutes").val(Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)));
-            $("#Seconds").val(Math.floor((difference % (1000 * 60)) / 1000));
-            $("#MilliSeconds").val(difference % 1000);
+    display.innerText=`${hour} : ${minutes} : ${seconds}`
+}
+
+function formatTime(time) {
+    if ( time < 10 ) {
+        return '0' + time;
+    }
+    return time;
+}
+
+function setAlarmTime(value) {
+    alarmTime = value;
+}
+
+function setAlarm() {
+    if(alarmTime) {
+        const current = new Date();
+        const timeToAlarm = new Date(alarmTime);
+
+        if (timeToAlarm > current) {
+            const timeout = timeToAlarm.getTime() - current.getTime();
+            alarmTimeout = setTimeout(() => audio.play(), timeout);
+            alert('Alarm set');
         }
-    })
+    }
+}
 
+function clearAlarm() {
+    audio.pause();
+    if (alarmTimeout) {
+        clearTimeout(alarmTimeout);
+        alert('Alarm cleared');
+    }
+}
 
-    $("#Start").click(function () {
-        if (startBtnstate == 0) {
-            startBtnstate = startBtnstate + 1;
-            $("#Start").html("Pause");
-            if (manualstate == 1) {
-                countdownMilliSeconds = $("#Days").val() * (1000 * 60 * 60 * 24) +
-                        $("#Hours").val() * (1000 * 60 * 60) +
-                        $("#Minutes").val() * (1000 * 60) +
-                        $("#Seconds").val() * 1000;
-                manualstate = 0;
-            } else {
-                var now = new Date();
-                countdownMilliSeconds = (countDownDateTime - now);
-            }
-            if (countdownMilliSeconds <= 0) {
-                $("#Start").html("Start");
-                startBtnstate = 0;
-                return;
-            }
-            x = setInterval(function () {
-                countdownMilliSeconds -= 10;
-                // Time calculations for days, hours, minutes and seconds
-                var days = Math.floor(countdownMilliSeconds / (1000 * 60 * 60 * 24));
-                var hours = Math.floor((countdownMilliSeconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                var minutes = Math.floor((countdownMilliSeconds % (1000 * 60 * 60)) / (1000 * 60));
-                var seconds = Math.floor((countdownMilliSeconds % (1000 * 60)) / 1000);
-                var milliseconds = countdownMilliSeconds % (1000);
-
-                $("#Days").val(days);
-                $("#Hours").val(hours);
-                $("#Minutes").val(minutes);
-                $("#Seconds").val(seconds);
-                $("#MilliSeconds").val(milliseconds);
-                if (countdownMilliSeconds < 0) {
-                    $("#Days").val(0);
-                    $("#Hours").val(0);
-                    $("#Minutes").val(0);
-                    $("#Seconds").val(0);
-                    $("#MilliSeconds").val(0);
-                    clearInterval(x);
-                    $("#Start").html("Quiet");
-                    audioElement.play();
-                    startBtnstate = 3;
-                    return;
-                }
-            }, 10);
-        } else if (startBtnstate == 1) { // Pause
-            startBtnstate = 2;
-            clearInterval(x);
-            $("#Start").html("Resume");
-        } else if (startBtnstate == 2) {
-            startBtnstate = 1;
-            $("#Start").html("Pause");
-            x = setInterval(function () {
-                countdownMilliSeconds -= 10;
-
-                var days = Math.floor(countdownMilliSeconds / (1000 * 60 * 60 * 24));
-                var hours = Math.floor((countdownMilliSeconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                var minutes = Math.floor((countdownMilliSeconds % (1000 * 60 * 60)) / (1000 * 60));
-                var seconds = Math.floor((countdownMilliSeconds % (1000 * 60)) / 1000);
-                var milliseconds = countdownMilliSeconds % (1000);
-
-                $("#Days").val(days);
-                $("#Hours").val(hours);
-                $("#Minutes").val(minutes);
-                $("#Seconds").val(seconds);
-                $("#MilliSeconds").val(milliseconds);
-                // Countdown over, report and ring
-                if (countdownMilliSeconds < 0) {
-                    $("#Days").val(0);
-                    $("#Hours").val(0);
-                    $("#Minutes").val(0);
-                    $("#Seconds").val(0);
-                    $("#MilliSeconds").val(0);
-                    clearInterval(x);
-                    $("#Start").html("Reset");
-                    audioElement.play();
-                    startBtnstate = 3;
-                    return;
-                }
-            }, 10);
-        } else if (startBtnstate == 3) {
-            audioElement.pause();
-            startBtnstate = 0;
-            clearInterval(x);
-            location.reload();
-            $("#Start").html("Start");
-        }
-    })
-    $("#Snooze").click(function () {
-        audioElement.pause();
-        countdownMilliSeconds = 10000;
-        $("#Days").val(0);
-        $("#Hours").val(0);
-        $("#Minutes").val(0);
-        $("#Seconds").val(10);
-        $("#MilliSeconds").val(0);
-        x = setInterval(function () {
-            countdownMilliSeconds -= 10;
-            days = Math.floor(countdownMilliSeconds / (1000 * 60 * 60 * 24));
-            hours = Math.floor((countdownMilliSeconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            minutes = Math.floor((countdownMilliSeconds % (1000 * 60 * 60)) / (1000 * 60));
-            seconds = Math.floor((countdownMilliSeconds % (1000 * 60)) / 1000);
-            milliseconds = countdownMilliSeconds % (1000);
-            $("#Days").val(days);
-            $("#Hours").val(hours);
-            $("#Minutes").val(minutes);
-            $("#Seconds").val(seconds);
-            $("#MilliSeconds").val(milliseconds);
-            if (countdownMilliSeconds < 0) {
-                $("#Days").val(0);
-                $("#Hours").val(0);
-                $("#Minutes").val(0);
-                $("#Seconds").val(0);
-                $("#MilliSeconds").val(0);
-                clearInterval(x);
-                $("#Start").html("Cancel");
-                audioElement.play();
-                startBtnstate = 3;
-                return;
-            }
-        }, 10);
-    })
-    $("#Reset").click(function () {
-        location.reload();
-    })
-
-})
+setInterval(updateTime, 1000);
